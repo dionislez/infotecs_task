25d7d8850a9f05b168a252ced8e99a0bdd5badda
Denis <leznevskiy@mail.ru>
2023-02-17 08:07:20 +0700

    feat(server): added tags changed docs and redoc endpoints and added decsriptions

--- server.py
+++ server.py
@@ -1,82 +1,80 @@
 from fastapi import FastAPI, Path, Query
+from fastapi.responses import JSONResponse
 
 import queries
 
 
-app: FastAPI = FastAPI()
+app: FastAPI = FastAPI(
+    docs_url='/infotecs/docs',
+    redoc_url='/infotecs/redoc',
+    version='TASK',
+    title='Infotecs task. Leznevskiy Denis.'
+)
 
 
-@app.get('/')
-async def main() -> dict:
-    '''http://127.0.0.1:8000/docs'''
-    return {
-        'swagger': 'http://127.0.0.1:8000/docs',
-        'get_city_by_geonameid': 'http://127.0.0.1:8000/cities/{geonameid}',
-        'get_cities_by_page_and_count': 'http://127.0.0.1:8000/cities?page={page}&count={count}',
-        'compare_two_cities': 'http://127.0.0.1:8000/cities/compare/{first_city}&{second_city}',
-        'find_available_cities': 'http://127.0.0.1:8000/cities/find/{hint}',
-        'made_by': 'Leznevskiy Denis'
-    }
-
-
-@app.get('/cities/{geonameid}')
+@app.get('/cities/{geonameid}', tags=['1) Cities by a geonameid'])
 async def get_city_by_geonameid(
-    geonameid: str = Path(min_length=6, max_length=8)
+    geonameid: int = Path(gt=0, description='example: 1489425')
 ) -> dict:
-    '''http://127.0.0.1:8000/cities/{geonameid}'''
-    try:
-        int(geonameid)
-    except ValueError:
-        return {'Error': 'There is no such geonameid'}
-
-    found_city: dict | bool = await queries.cities_find_by_geonameid(geonameid)
+    '''/cities/{geonameid}'''
+    found_city: dict | bool = await queries.cities_find_by_geonameid(str(geonameid))
     if not found_city:
-        found_city: dict = {
-            'Error': f'the city with {geonameid} geonameid was not found'
-        }
-
+        return JSONResponse(
+            content={'message': 'Wrong geonameid'},
+            status_code=404
+        )
     return found_city
 
 
-@app.get('/cities')
+@app.get('/cities', tags=['2) Cities by a page and a count'])
 async def get_cities_by_page_and_count(
-    page: int = Query(ge=0.1),
-    count: int = Query(ge=0.1, le=500.1)
+    page: int = Query(gt=0, description='example: 1'),
+    count: int = Query(gt=0, lt=501, description='example: 5')
 ) -> queries.Union[dict, list]:
-    '''http://127.0.0.1:8000/cities?page={page}&count={count}'''
+    '''/cities?page={page}&count={count}'''
     found_cities: tuple = await queries.cities_by_page_count(page, count)
-
     if not found_cities[0]:
-        return {'Error': f'{found_cities[1]} pages available for {count} cities'}
-
+        return JSONResponse(
+            content={'message': f'{found_cities[1]} pages for {count} cities'},
+            status_code=404
+        )
     return found_cities[0]
 
 
-@app.get('/cities/compare/{first_city}&{second_city}')
+@app.get(
+    '/cities/compare/{first_city}&{second_city}', tags=['3) First and second cities']
+)
 async def compare_two_cities(
-    first_city: str = Path(max_length=50),
-    second_city: str = Path(max_length=50)
+    first_city: str = Path(max_length=50, description='example: Томск'),
+    second_city: str = Path(max_length=50, description='example: Кемерово')
 ) -> dict:
-    '''http://127.0.0.1:8000/cities/compare/{first_city}&{second_city}'''
+    '''/cities/compare/{first_city}&{second_city}'''
     compared: bool | dict = await queries.cities_comparing(
         first_city.lower().replace('ë', 'e').replace('ё', 'е'),
         second_city.lower().replace('ë', 'e').replace('ё', 'е')
     )
     if not compared:
-        return {'Error': 'there is no such city/cities'}
-
+        return JSONResponse(
+            content={'message': 'No cities'},
+            status_code=404
+        )
     return compared
 
 
-@app.get('/cities/find/{hint}')
+@app.get(
+    '/cities/find/{hint}',
+    tags=['4) Find cities by a hint']
+)
 async def find_available_cities(
-    hint: str = Path(min_length=1, max_length=20)
+    hint: str = Path(max_length=20, description='example: Томск')
 ) -> dict:
-    '''http://127.0.0.1:8000/cities/find/{hint}'''
+    '''/cities/find/{hint}'''
     find_hints: bool | dict = await queries.cities_help_hints(
         hint.lower().replace('ë', 'e').replace('ё', 'е')
     )
     if not find_hints:
-        return {'Error': 'there are no cities with such hint'}
-
+        return JSONResponse(
+            content={'message': 'No cities'},
+            status_code=404
+        )
     return find_hints
